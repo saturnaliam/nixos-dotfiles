81b2382669a96d12a46dba5bebe56cdf5e6db8ac
saturnaliam <luciacdev@gmail.com>
2024-03-25 22:34:02 -0500

    nvim stuff i think i added snippets

--- nonnix/nvim/lua/plugins/lsp.lua
+++ nonnix/nvim/lua/plugins/lsp.lua
@@ -1,10 +1,53 @@
 return {
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	{'neovim/nvim-lspconfig',
+	{ 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' }, -- forget mason this is sillier
+	{ 'neovim/nvim-lspconfig',
 		config = function()
-			require'lspconfig'.lua_ls.setup{}
-		end},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{'L3MON4D3/LuaSnip'},
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			require'lspconfig'.lua_ls.setup({
+        capabilities = capabilities
+      })
+
+      require'lspconfig'.clangd.setup({
+        capabilities = capabilities
+      })
+		end
+  },
+	{ 'hrsh7th/cmp-nvim-lsp' },
+	{ 'hrsh7th/nvim-cmp',
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
+      }, {
+        { name = 'buffer' },
+      })
+    })
+   end,
+  },
+	{ 'L3MON4D3/LuaSnip',
+    dependencies = {
+      'saadparwaiz1/cmp_luasnip',
+      'rafamadriz/friendly-snippets',
+    },
+  },
 }
