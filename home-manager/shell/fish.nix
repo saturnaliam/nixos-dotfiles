8f1728a24381b59330b4b7428bd447e7e5db7f86
saturnaliam <luciacdev@gmail.com>
2024-03-26 13:23:06 -0500

    changed the manpage fellow to neovim

--- home-manager/shell/fish.nix
+++ home-manager/shell/fish.nix
@@ -9,6 +9,8 @@
 			function fish_prompt
 				printf ' %s%s%s%s %s><> ' (set_color $fish_color_command) (prompt_pwd) (set_color $fish_color_cwd) (fish_git_prompt) (set_color $fish_color_keyword)
 			end
+
+      set -x MANPAGER "nvim +Man!"
 		'';
 
 		shellAliases = {
