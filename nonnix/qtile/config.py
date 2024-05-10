975dd26285cd20cb43ddd885ac2fdc11aa947b88
saturnaliam <luciacdev@gmail.com>
2024-05-09 23:26:01 -0500

    configuring the silly bar in qtile + making some windows float by default

--- nonnix/qtile/config.py
+++ nonnix/qtile/config.py
@@ -2,7 +2,7 @@ import socket
 import os
 import subprocess
 from libqtile import bar, layout, qtile, widget, hook
-from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.config import Click, Drag, Group, Key, KeyChord, Match, Screen
 from libqtile.lazy import lazy
 
 base = "#191724"
@@ -32,18 +32,20 @@ keys = [
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key(
+        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "equal", lazy.layout.grow(), desc="Grow window"),
+    Key([mod], "minus", lazy.layout.shrink(), desc="Shrink window"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-
     Key(
         [mod, "shift"],
         "Return",
@@ -51,7 +53,6 @@ keys = [
         desc="Toggle between split and unsplit sides of stack",
     ),
     Key([mod], "t", lazy.spawn(terminal), desc="Launch terminal"),
-
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "c", lazy.window.kill(), desc="Kill focused window"),
     Key(
@@ -60,13 +61,20 @@ keys = [
         lazy.window.toggle_fullscreen(),
         desc="Toggle fullscreen on the focused window",
     ),
-    Key([mod], "v", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key(
+        [mod],
+        "v",
+        lazy.window.toggle_floating(),
+        desc="Toggle floating on the focused window",
+    ),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
     Key([mod], "d", lazy.spawn("dmenu_run -h 30"), desc="spawn dmenu"),
     Key([mod], "f", lazy.spawn("firefox"), desc="spawn firefox"),
     Key([mod, "shift"], "e", lazy.spawn("emacsclient -c"), desc="spawn emacs"),
-    Key([mod, "shift"], "g", lazy.spawn(f"{terminal} -e tmux attach"), desc="spawn tmux"),
+    Key(
+        [mod, "shift"], "g", lazy.spawn(f"{terminal} -e tmux attach"), desc="spawn tmux"
+    ),
     Key([mod], "g", lazy.spawn(f"{terminal} -e tmux"), desc="spawn new tmux session"),
 ]
 
@@ -127,42 +135,51 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font="Iosevka NFM",
+    font="Iosevka Nerd Font Propo",
     fontsize=13,
     padding=3,
-    background = base,
+    background=base,
 )
 extension_defaults = widget_defaults.copy()
+
 
 def get_bar(screen_num):
     widgets = [
         widget.GroupBox(
-            margin_y = 5,
-            margin_x = 7,
-            padding_y = 0,
-            padding_x = 1,
-            highlight_method = "line",
-            highlight_color = highlight_low,
-            active = iris,
-            foreground = text,
-            inactive = highlight_high,
-            this_current_screen_border = rose,
-            borderwidth = 3,
+            margin_y=5,
+            margin_x=7,
+            padding_y=0,
+            padding_x=1,
+            highlight_method="line",
+            highlight_color=highlight_low,
+            active=iris,
+            foreground=text,
+            inactive=highlight_high,
+            this_current_screen_border=rose,
+            borderwidth=3,
         ),
-        widget.TextBox(text = "|", foreground = muted),
-        widget.WindowName(max_chars = 40),
-        widget.Spacer(length = 8),
-        widget.Battery(format = "󰁹 {percent:2.0%}", foreground = foam, low_foreground = love),
-        widget.TextBox(text = "|", foreground = muted),
-        widget.Wlan(format = " {essid}", interface = "wlp2s0", foreground = gold),
-        widget.TextBox(text = "|", foreground = muted),
-        widget.Clock(
-            foreground = iris,
-            format = "%H:%M"
+        widget.TextBox(text="|", foreground=muted),
+        widget.WindowName(max_chars=40),
+        widget.Spacer(length=8),
+        widget.PulseVolume(foreground=foam, fmt=" {}"),
+        widget.TextBox(text="|", foreground=muted),
+        widget.Battery(
+            format="󰁹 {percent:2.0%}", foreground="#a6e3a1", low_foreground=love
         ),
-        widget.TextBox(text = "|", foreground = muted),
-        widget.Systray(padding = 3),
-        widget.Spacer(length = 8),
+        widget.TextBox(text="|", foreground=muted),
+        widget.Wlan(format=" {essid}", interface="wlp2s0", foreground=gold),
+        widget.TextBox(text="|", foreground=muted),
+        widget.Clock(foreground=iris, format="%H:%M"),
+        widget.TextBox(text="|", foreground=muted),
+        widget.Mpris2(
+            format="{xesam:title} - {xesam:artist}",
+            no_metadata_text="Unknown track",
+            paused_text=" {track}",
+            playing_text=" {track}",
+            width=150,
+            foreground=foam,
+        ),
+        widget.Spacer(length=8),
     ]
 
     if is_desktop:
@@ -172,15 +189,25 @@ def get_bar(screen_num):
         del widgets[-3:]
     return bar.Bar(widgets, 30)
 
+
 screens = [
     Screen(top=get_bar(1)),
-    Screen(top=get_bar(2)),
 ]
+
+if is_desktop:
+    screens.append(Screen(top=get_bar(2)))
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag(
+        [mod],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
+    Drag(
+        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
+    ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
@@ -191,6 +218,8 @@ bring_front_click = False
 floats_kept_above = True
 cursor_warp = False
 floating_layout = layout.Floating(
+    border_focus=rose,
+    border_normal=highlight_high,
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
@@ -200,7 +229,9 @@ floating_layout = layout.Floating(
         Match(wm_class="ssh-askpass"),  # ssh-askpass
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
-    ]
+        Match(title="Spotify"),
+        Match(wm_class="KeePassXC"),
+    ],
 )
 auto_fullscreen = True
 focus_on_window_activation = "smart"
@@ -213,10 +244,12 @@ auto_minimize = True
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
 
+
 @hook.subscribe.startup_once
 def start_once():
-    home = os.path.expanduser('~')
-    subprocess.call('/etc/nixos/scripts/autostart-xorg.sh')
+    home = os.path.expanduser("~")
+    subprocess.call("/etc/nixos/scripts/autostart-xorg.sh")
+
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
